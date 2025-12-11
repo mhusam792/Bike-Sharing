1e0cef78744cf993efb15f18fd7dc20b272371bc
Mohamed Hussam <mhussam792@gmail.com>
2025-12-11 19:57:45 +0200

    modifing files

--- src/predict.py
+++ src/predict.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import joblib
 
-def predict_new_data(X_new: pd.DataFrame, model_path='../models/catboost_pipeline.pkl', save_path='../predictions/predicted_results.csv'):
+def predict_new_data(X_new: pd.DataFrame, model_path='models/catboost_pipeline.pkl', save_path='predictions/predicted_results.csv'):
     model_pipeline = joblib.load(model_path)
     y_pred = model_pipeline.predict(X_new)
     X_new['predicted_cnt'] = y_pred
