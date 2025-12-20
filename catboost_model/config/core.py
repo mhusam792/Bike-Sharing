c9a053524ca44bdf1d94fa3024f205352de5d3cc
Mohamed Hussam <mhussam792@gmail.com>
2025-12-20 20:43:07 +0200

    Creating config.yml file And Making processing/data_manager.py for manage data

---
+++ catboost_model/config/core.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+from typing import List, Optional
+
+from pydantic import BaseModel, ConfigDict
+from strictyaml import YAML, load
+
+import catboost_model
+
+# Project Directories
+PACKAGE_ROOT = Path(catboost_model.__file__).resolve().parent
+ROOT = PACKAGE_ROOT.parent
+CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
+DATASET_DIR = PACKAGE_ROOT / "datasets"
+TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+
+class AppConfig(BaseModel):
+    """
+    Application-level config
+    """
+
+    package_name: str
+    training_data_file: str
+    test_data_file: str
+    pipeline_save_file: str
+
+class ModelConfig(BaseModel):
+    """
+    All configuration relevant to model
+    training and feature engineering.
+    """
+
+    target: str
+    features: List[str]
+    test_size: float
+    random_state: int
+    cyclical_cols: List[str]
+    num_cols: list[str]
+    cat_cols: List[str]
+    new_features: list[str]
+    
+class Settings(BaseModel):
+    """Master config object."""
+
+    app_config: AppConfig
+    model_config_features: ModelConfig
+
+    model_config = ConfigDict(extra="forbid")
+
+
+def find_config_file() -> Path:
+    """Locate the configuration file."""
+    if CONFIG_FILE_PATH.is_file():
+        return CONFIG_FILE_PATH
+    raise Exception(f"Config not found at {CONFIG_FILE_PATH!r}")
+
+
+def fetch_config_from_yaml(cfg_path: Optional[Path] = None) -> YAML:
+    """Parse YAML containing the package configuration."""
+
+    if not cfg_path:
+        cfg_path = find_config_file()
+
+    if cfg_path:
+        with open(cfg_path, "r") as conf_file:
+            parsed_config = load(conf_file.read())
+            return parsed_config
+    raise OSError(f"Did not find config file at path: {cfg_path}")
+
+
+def create_and_validate_config(parsed_config: YAML = None) -> Settings:
+    """Run validation on config values."""
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml()
+
+    # specify the data attribute from the strictyaml YAML type.
+    _config = Settings(
+        app_config=AppConfig(**parsed_config.data),
+        model_config_features=ModelConfig(**parsed_config.data),
+    )
+
+    return _config
+
+
+config = create_and_validate_config()
