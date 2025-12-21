634c980011454b96ff9353299065b74b79ca1e81
Mohamed Hussam <mhussam792@gmail.com>
2025-12-21 20:56:11 +0200

    Setting up paths for packaging

--- bike_sharing_regressor_model/bike_sharing_regressor_model/config/settings.py
+++ bike_sharing_regressor_model/bike_sharing_regressor_model/config/settings.py
@@ -1,5 +1,22 @@
 from bike_sharing_regressor_model.utils.helper import load_config
+import bike_sharing_regressor_model
+from pathlib import Path
 
 
-DATA_CONFIG_PATH = "configs/data.yml"
+PACKAGE_ROOT = Path(bike_sharing_regressor_model.__file__).resolve().parent
+ROOT = PACKAGE_ROOT.parent
+
+DATA_CONFIG_PATH = ROOT / "configs" / "data.yml"
 DATA_CONFIG = load_config(DATA_CONFIG_PATH)
+
+TRAINING_DATA_FILE_PATH = ROOT / "datasets" / DATA_CONFIG['training_data_file']
+TESTING_DATA_FILE_PATH = ROOT / "datasets" / DATA_CONFIG['test_data_file']
+
+DATASET_DIR = ROOT / "datasets"
+TRAINED_MODEL_PATH = ROOT / "trained_models" / DATA_CONFIG['pipeline_save_file']
+
+FEATURES_LIST = DATA_CONFIG['features']
+TARGET = DATA_CONFIG['target']
+
+TEST_SIZE = DATA_CONFIG['split']['test_size']
+RANDOM_STATE = DATA_CONFIG['split']['random_state']
