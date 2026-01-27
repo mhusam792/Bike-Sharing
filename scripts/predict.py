fefb819f188e11b0883ddc603696bd18a56771c3
Mohamed Hussam <mhussam792@gmail.com>
2026-01-27 21:45:38 +0200

    Untrach scripts and tests directories

--- scripts/predict.py
+++
@@ -1,4 +0,0 @@
-from bike_sharing_model.models.predictor import run_prediction
-
-if __name__ == "__main__":
-    run_prediction()
--- scripts/train.py
+++
@@ -1,6 +0,0 @@
-# from bike_sharing_model.models.trainer import run_training
-from bike_sharing_model.models.trainer import run_training
-
-
-if __name__ == "__main__":
-    run_training()
